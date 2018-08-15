951228a663025a0935599ed58fe11422348b4169
sthai1721 <sophia.thai@rutgers.edu>
2018-08-15 15:33:10 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,11 @@
-class Model
-  # Replace with Dog clas
+class Dog(name, breed, age)
+  
+  def initialization
+    @name= name
+    @breed= breed
+    @age= age
+    initialization.all
+  end
+  
+
 end
